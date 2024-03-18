7383cfdecad167738574ba00c8ea91f9db21a878
SamBridgess <ilya.minyaeff@yandex.ru>
2024-03-18 12:42:18 +0300

    visualization fix

--- src/test/resources/result/graph.py
+++ src/test/resources/result/graph.py
@@ -1,23 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Read the CSV file
-file_path = 'log/ln.csv'  # Update with your CSV file path
+file_path = 'FuncSystem.csv'
 df = pd.read_csv(file_path, names=['Input', 'Output'])
 
-# Get Input and Output columns
 x = df['Input']
 y = df['Output']
 
-# Create a plot
 plt.figure(figsize=(8, 6))
 plt.plot(x, y, marker='o', linestyle='-', color='b', label='Function')
 plt.xlabel('Input')
 plt.ylabel('Output')
-plt.title('Function Visualization')
+plt.title(file_path)
 plt.legend()
 
-# Display the plot
 plt.grid(True)
 plt.tight_layout()
 plt.show()
